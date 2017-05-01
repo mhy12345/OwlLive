b0bd6e253b640d68430d319a577a47245cd85b0b
root <root@dfc87bb1fa43.(none)>
2017-05-01 06:18:27 +0000

    docker version

--- models/user.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var UserSchema = new mongoose.Schema({
-	name:String,
-	password:String,
-	level:String,
-	lastAction:Date,
-	registerTime:Date
-});
-module.exports=mongoose.model('users', UserSchema);
